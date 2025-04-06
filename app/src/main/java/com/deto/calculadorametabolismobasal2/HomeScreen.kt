e88e5ed2c71c1e758fd4469afd2277a1a048c8c1
detossj <jorgerubilarssj@gmail.com>
2025-04-06 14:18:27 -0400

    HomeScreen.kt, NavController.kt, SecondScreen.kt create and dependecies added

---
+++ app/src/main/java/com/deto/calculadorametabolismobasal2/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.deto.calculadorametabolismobasal2
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun HomeScreen( navController: NavController ){
+
+}
---
+++ app/src/main/java/com/deto/calculadorametabolismobasal2/NavController.kt
@@ -0,0 +1,29 @@
+package com.deto.calculadorametabolismobasal2
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import kotlinx.serialization.Serializable
+
+@Serializable
+object Home
+
+@Serializable
+data class SecondScreen(val resultado: Double)
+
+@Composable
+fun Navigation(){
+    val navController = rememberNavController()
+
+    NavHost( navController = navController, startDestination = Home){
+        composable<Home> {
+            HomeScreen(navController = navController)
+        }
+        composable<SecondScreen> { backStackEntry ->
+            val args = backStackEntry.toRoute<SecondScreen>()
+            SecondScreen(navController = navController, resultado = args.resultado)
+        }
+    }
+}
