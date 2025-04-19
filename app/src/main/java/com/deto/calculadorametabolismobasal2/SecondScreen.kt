5fa3cd5410917ca181866467c5d40870cc6fc265
detossj <jorgerubilarssj@gmail.com>
2025-04-19 19:55:57 -0400

    Card added

--- app/src/main/java/com/deto/calculadorametabolismobasal2/SecondScreen.kt
+++ app/src/main/java/com/deto/calculadorametabolismobasal2/SecondScreen.kt
@@ -1,9 +1,60 @@
 package com.deto.calculadorametabolismobasal2
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.deto.calculadorametabolismobasal2.ui.theme.BackGround
+
 
 @Composable
 fun SecondScreen(navController: NavController, resultado: Double){
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(BackGround)
+    ) {
+
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Card(
+                modifier = Modifier.fillMaxWidth().padding(top = 30.dp).padding(horizontal = 12.dp)
+
+            ) {
+
+                Column(
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Text(
+                        text = "Tu Metabolismo Basal es de",
+                        fontSize = 16.sp
+                    )
+                    Text(
+                        text = "${resultado} Kcal/dia",
+                        fontSize = 32.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+
+
+            }
+        }
+    }
 
 }
